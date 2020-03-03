b288ba9b47fe59267d96b742848431a706125f30
zhangyan <zhangyan@zhangkongapp.com>
2020-03-03 15:35:17 +0800

    新增函数打分搜索

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,10 +2,17 @@ package org.example;
 
 import com.alibaba.fastjson.JSON;
 import org.apache.http.HttpHost;
+import org.apache.lucene.queries.function.FunctionQuery;
+import org.apache.lucene.search.join.ScoreMode;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.lucene.search.function.CombineFunction;
+import org.elasticsearch.common.lucene.search.function.FunctionScoreQuery;
 import org.elasticsearch.index.query.*;
+import org.elasticsearch.index.query.functionscore.FunctionScoreQueryBuilder;
+import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilder;
+import org.elasticsearch.index.query.functionscore.WeightBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.example.es.plugin.EsUtils;
 import org.example.es.plugin.IndexBuilder;
@@ -33,32 +40,15 @@ public class App
                 new HttpHost(elasticsProperties.getClusterNodes(), elasticsProperties.getPort()),
                 elasticsProperties.getUserName(), elasticsProperties.getPassword()).getRestHighLevelClient();
 
-        BoolQueryBuilder boolBuilder = QueryBuilders.boolQuery();
-        MatchQueryBuilder matchQueryBuilder = QueryBuilders.matchQuery(FILED, "部落冲突");
-        QueryStringQueryBuilder queryStringQueryBuilder = QueryBuilders.queryStringQuery("部落冲突");
-        queryStringQueryBuilder.defaultField("searchInfo.appAnalyzerName");
-        queryStringQueryBuilder.queryName("部落冲突");
+
+//        SearchSourceBuilder searchSourceBuilder = EsUtils.listByQueryBuild(getNestedQueryBuilder(), "");
+
+        SearchSourceBuilder searchSourceBuilder = EsUtils.pageByQueryBuild(getFunctionScoreQueryBuilder(), 1, 100,"");
 
 
-        DisMaxQueryBuilder disMaxQueryBuilder = QueryBuilders.disMaxQuery()
-                .add(QueryBuilders.termQuery(FILED, "部落冲突"))
-                .add(QueryBuilders.termQuery(FILED, "部落冲"))
-                .add(QueryBuilders.termQuery(FILED, "部落"))
-                .boost(1.2f)
-                .tieBreaker(0.7f);
-
-
-        String name ="部落冲突";
-        String regex = "[^a-zA-Z0-9\\u4E00-\\u9FA5]";
-        name = name.replaceAll(regex, "").toLowerCase();
-//        NestedQueryBuilder nestedQueryBuilder = QueryBuilders.nestedQuery("searchInfo", QueryBuilders.termQuery("searchInfo.appAnalyzerName", name), ScoreMode.None);
-//        nestedQueryBuilder.query().queryName();
-
-//        boolBuilder.must(nestedQueryBuilder);
-
-        SearchSourceBuilder searchSourceBuilder = EsUtils.listByQueryBuild(matchQueryBuilder, "");
         SearchRequest searchRequest = EsUtils.getSearchRequest(searchSourceBuilder,
                 "index_app_info_v4_test", IndexBuilder.getType(AppInfo.class));
+
 
         SearchResponse response = EsUtils.getSearchResponse(restHighLevelClient, searchRequest);
 
@@ -68,5 +58,55 @@ public class App
         }
 
 
+    }
+
+    public static FunctionScoreQueryBuilder getFunctionScoreQueryBuilder() {
+        BoolQueryBuilder boolBuilder = QueryBuilders.boolQuery();
+
+        FunctionScoreQueryBuilder.FilterFunctionBuilder[] filterFunctionBuilders = new FunctionScoreQueryBuilder.FilterFunctionBuilder[3];
+        ScoreFunctionBuilder<WeightBuilder> scoreFunctionBuilder = new WeightBuilder();
+        scoreFunctionBuilder.setWeight(5000);
+        NestedQueryBuilder nestedQueryBuilder = QueryBuilders.nestedQuery("searchInfo", QueryBuilders.termQuery(FILED, "植物大战僵尸2天空之城"), ScoreMode.None);
+        FunctionScoreQueryBuilder.FilterFunctionBuilder category = new FunctionScoreQueryBuilder.FilterFunctionBuilder(nestedQueryBuilder, scoreFunctionBuilder);
+        filterFunctionBuilders[0] = category;
+
+        ScoreFunctionBuilder<WeightBuilder> scoreFunctionBuilder1 = new WeightBuilder();
+        scoreFunctionBuilder1.setWeight(30);
+        NestedQueryBuilder nestedQueryBuilder1 = QueryBuilders.nestedQuery("searchInfo", QueryBuilders.termQuery(FILED, "植物大战僵尸2"), ScoreMode.None);
+        FunctionScoreQueryBuilder.FilterFunctionBuilder category1 = new FunctionScoreQueryBuilder.FilterFunctionBuilder(nestedQueryBuilder1, scoreFunctionBuilder);
+        filterFunctionBuilders[1] = category1;
+
+        ScoreFunctionBuilder<WeightBuilder> scoreFunctionBuilder2 = new WeightBuilder();
+        scoreFunctionBuilder2.setWeight(1);
+        NestedQueryBuilder nestedQueryBuilder2 = QueryBuilders.nestedQuery("searchInfo", QueryBuilders.termQuery(FILED, "植物大战"), ScoreMode.None);
+        FunctionScoreQueryBuilder.FilterFunctionBuilder category2 = new FunctionScoreQueryBuilder.FilterFunctionBuilder(nestedQueryBuilder2, scoreFunctionBuilder);
+        filterFunctionBuilders[2] = category2;
+
+        FunctionScoreQueryBuilder query = QueryBuilders.functionScoreQuery(filterFunctionBuilders)
+                .scoreMode(FunctionScoreQuery.ScoreMode.SUM).boostMode(CombineFunction.SUM);
+        return query;
+    }
+
+    public static BoolQueryBuilder getQueryStringBuilder() {
+        BoolQueryBuilder boolBuilder = QueryBuilders.boolQuery();
+        QueryStringQueryBuilder queryStringQueryBuilder = QueryBuilders.queryStringQuery("部落冲突");
+        queryStringQueryBuilder.defaultField(FILED);
+        NestedQueryBuilder nestedQueryBuilder = QueryBuilders.nestedQuery("searchInfo", queryStringQueryBuilder, ScoreMode.None);
+        boolBuilder.must(nestedQueryBuilder);
+        return boolBuilder;
+    }
+
+    public static BoolQueryBuilder getNestedQueryBuilder() {
+        BoolQueryBuilder boolBuilder = QueryBuilders.boolQuery();
+        BoolQueryBuilder nestedBoolQueryBuilder = QueryBuilders.boolQuery();
+        nestedBoolQueryBuilder.must(QueryBuilders.matchQuery(FILED, "部落冲突"));
+
+        String name ="部落冲突";
+        String regex = "[^a-zA-Z0-9\\u4E00-\\u9FA5]";
+        name = name.replaceAll(regex, "").toLowerCase();
+        NestedQueryBuilder nestedQueryBuilder = QueryBuilders.nestedQuery("searchInfo", nestedBoolQueryBuilder, ScoreMode.None);
+        nestedQueryBuilder.query().queryName();
+        boolBuilder.must(nestedQueryBuilder);
+        return boolBuilder;
     }
 }
